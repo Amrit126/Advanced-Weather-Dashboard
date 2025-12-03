1b01ebc8f3b46d2e92c4d22e4ef31421710f3909
Amrit Paudel <amritpoudel0100@gmail.com>
2025-12-03 19:49:08 +0545

    Fetching data and rendering properly on Favorites Card

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -1,6 +1,41 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import { Header } from '../../components/Header'
+import { FavoritesCard } from './FavoritesCard'
 
-export function Favorites({ api, favorites, setFavorites }) {
+export function Favorites({ apiKey, favorites, setFavorites }) {
+    const [cities, setCities] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const promises = favorites.map(async (favorite) => {
+                const url = `https://api.openweathermap.org/data/2.5/weather?q=${favorite}&appid=${apiKey}&units=metric`
+                const response = await axios.get(url)
+                const data = response.data
+                return {
+                    name: data.name,
+                    icon: data,
+                    sunrise: data.sys.sunrise,
+                    sunset: data.sys.sunset,
+                    temp: data.main.temp,
+                    main: data.weather[0].main
+                }
+            })
+
+            try {
+                const results = await Promise.all(promises)
+
+                setCities(results)
+                console.log(results)
+            } catch (e) {
+                console.log(e.msg)
+            }
+        }
+
+        fetchData()
+
+    }, [favorites, setCities, apiKey])
+
     return (
         <>
             <Header />
@@ -8,60 +43,7 @@ export function Favorites({ api, favorites, setFavorites }) {
                 className="text-center py-8 text-5xl bg-gray-100 w-full font-extrabold"
             >Favorites</h2>
             <div className="h-full bg-gray-100 flex flex-wrap gap-8 p-12">
-
-                <div className="flex flex-col items-center justify-center bg-linear-to-r from-blue-600 to-blue-400
-                gap-2 p-2 text-2xl font-bold rounded-2xl shadow-black shadow-2xl">
-                    <div className="flex justify-between w-full">
-                        <div></div>
-                        <p>Kathmandu</p>
-                        <button>X</button>
-                    </div>
-                    <img src="/weather-icons/default.png" alt="Image Icon" />
-                    <p>27 *C</p>
-                    <p>Clouds</p>
-                    <div className="flex gap-2">
-                        <button className="bg-black p-4 text-white rounded-lg">More Details</button>
-                        <button className="bg-black p-4 text-white rounded-lg">View Forecast</button>
-                    </div>
-                </div>
-
-                <div className="flex flex-col items-center justify-center bg-linear-to-r from-blue-600 to-blue-400
-                gap-2 p-2 text-2xl font-bold rounded-2xl shadow-black shadow-2xl">
-                    <div className="flex justify-between w-full">
-                        <div></div>
-                        <p>Kathmandu</p>
-                        <button>X</button>
-                    </div>
-                    <img src="/weather-icons/default.png" alt="Image Icon" />
-                    <p>27 *C</p>
-                    <p>Clouds</p>
-                    <div className="flex gap-2">
-                        <button className="bg-black p-4 text-white rounded-lg">More Details</button>
-                        <button className="bg-black p-4 text-white rounded-lg">View Forecast</button>
-                    </div>
-                </div>
-
-                <div className="flex flex-col items-center justify-center bg-linear-to-r from-blue-600 to-blue-400
-                gap-2 p-2 text-2xl font-bold rounded-2xl shadow-black shadow-2xl transition-all duration-300
-                hover:scale-102">
-                    <div className="flex justify-between w-full">
-                        <div></div>
-                        <p>Kathmandu</p>
-                        <button
-                            title="Remove from favorites"
-                            className="mr-2 cursor-pointer transition-all duration-300 hover:opacity-80">X</button>
-                    </div>
-                    <img src="/weather-icons/default.png" alt="Image Icon" />
-                    <p>27 *C</p>
-                    <p>Clouds</p>
-                    <div className="flex gap-2">
-                        <button className="bg-black p-4 text-white rounded-lg
-                        transition-all duration-300 hover:opacity-80 cursor-pointer hover:scale-102">More Details</button>
-                        <button className="bg-black p-4 text-white rounded-lg
-                        transition-all duration-300 hover:opacity-80 cursor-pointer hover:scale-102">View Forecast</button>
-                    </div>
-                </div>
-
+                <FavoritesCard favorites={favorites} setFavorites={setFavorites} cities={cities} />
             </div>
         </>
     )
---
+++ src/pages/favorites/FavoritesCard.jsx
@@ -0,0 +1,39 @@
+import { getWeatherImage } from "../../utils/pickCorrectIcon"
+
+export function FavoritesCard({ favorites, setFavorites, cities }) {
+    return (
+        cities
+            ?
+            cities.map(city => {
+                return (
+                    <div key={city.sunrise} className="flex flex-col items-center justify-center bg-linear-to-r from-blue-600
+                     to-blue-400 gap-2 p-2 text-2xl font-bold rounded-2xl shadow-black shadow-2xl 
+                     transition-all duration-30 hover:scale-102">
+                        <div className="flex justify-between w-full">
+                            <div></div>
+                            <p>{city.name}</p>
+                            <button
+                                title="Remove from favorites"
+                                className="mr-2 cursor-pointer transition-all duration-300 hover:opacity-80">X</button>
+                        </div>
+                        <img src={`/weather-icons/${getWeatherImage(city.icon, city.sunrise, city.sunset)}`}
+                            alt="Image Icon" />
+                        <p>{city.temp} °C 🌡️</p>
+                        <p>{city.main}</p>
+                        <div className="flex gap-2">
+                            <button className="bg-black p-4 text-white rounded-lg
+                        transition-all duration-300 hover:opacity-80 cursor-pointer hover:scale-102"
+                            >More Details</button>
+                            <button className="bg-black p-4 text-white rounded-lg
+                        transition-all duration-300 hover:opacity-80 cursor-pointer hover:scale-102"
+                            >View Forecast</button>
+                        </div>
+                    </div>
+                )
+            })
+            :
+            <div>
+                <p>Loading...</p>
+            </div>
+    )
+}
